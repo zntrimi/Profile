ab6db9c99e964d62b73dc197d1b351f69c50f4c8
Zentaro Imai <zentaro0824@gmail.com>
2021-05-09 19:38:19 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Zentaro Imai on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
